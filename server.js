9836cb2731a19cf095eb0b13da9d432fc5e35842
sharebeezdev <107134955+sharebeezdev@users.noreply.github.com>
2024-09-16 19:08:23 +0530

    Update server.js
    Changed code to get the key from secret manager instead of local service key json file

--- server.js
+++ server.js
@@ -1,13 +1,30 @@
 const admin = require('firebase-admin');
 const express = require('express');
 const bodyParser = require('body-parser');
+const { SecretManagerServiceClient } = require('@google-cloud/secret-manager');
+const util = require('util');
 
-// Initialize the Firebase Admin SDK
-const serviceAccount = require('./serviceAccountKey.json');
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-});
+// Initialize the Secret Manager client
+const client = new SecretManagerServiceClient();
 
+// Function to access the secret
+async function accessSecretVersion(secretName) {
+  const [version] = await client.accessSecretVersion({ name: secretName });
+  const payload = version.payload.data.toString('utf8');
+  return JSON.parse(payload);
+}
+
+// Initialize Firebase Admin SDK
+async function initializeFirebase() {
+  const secretName = 'projects/fcm-server-435813/secrets/fem-service-configuration-key-json/versions/latest';
+  const serviceAccount = await accessSecretVersion(secretName);
+
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+  });
+}
+
+// Create and configure Express application
 const app = express();
 app.use(bodyParser.json());
 
@@ -30,8 +47,13 @@ app.post('/send', async (req, res) => {
   }
 });
 
-const port = process.env.PORT || 8080;
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+// Initialize Firebase and start the server
+initializeFirebase().then(() => {
+  const port = process.env.PORT || 8080;
+  app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+  });
+}).catch(error => {
+  console.error('Failed to initialize Firebase:', error);
+  process.exit(1);
 });
-
